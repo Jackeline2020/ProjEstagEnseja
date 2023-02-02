a3ebaa27c5f813b1cfebd4c7ad76f973a2dc1c4c
Jackeline Carvalho <jackeline.carvalho@global.ntt>
2023-02-02 11:12:46 -0300

    corrections changes

--- routes/skills.js
+++ routes/skills.js
@@ -2,13 +2,13 @@ var express = require('express');
 const router = express.Router();
 
 // include technologiesGSC controller
-const skillsGSC_controller = require('../controllers/skills.controller');
+const skillsGSC_controller = require('../service/skills.service');
 
 // routes
-router.get('/', skillsGSC_controller.list);  
+router.get('/', skillsGSC_controller.list);
 router.post('/create', skillsGSC_controller.create);
 router.get('/:id', skillsGSC_controller.show);
 router.post('/put/:id', skillsGSC_controller.update);
 router.post('/delete/:id', skillsGSC_controller.del);
-  
+
 module.exports = router;
